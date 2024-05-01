5f8cc4cac420cc9b4c9ce370379212d86b458615
vijayx21188955 <x21188955@student.ncirl.ie>
2024-05-02 00:50:28 +0100

    ommiot

---
+++ src/test/java/ApiTestCases/SampleTestCases.java
@@ -0,0 +1,40 @@
+package ApiTestCases;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class SampleTestCases {
+	
+	private static final String BASE_URL = "https://gorest.co.in/public/v2";
+    private static final String ACCESS_TOKEN = "c3025ece9f7715439cf26cc7cadb94244ca633b999ebb4fdae247c067ad64771";
+
+    @Test
+    public void testGetRequest() {
+        RestAssured.baseURI = "https://gorest.co.in/public/v2/";
+
+        given()
+            .contentType(ContentType.JSON)
+        .when()
+            .get("/users")
+        .then()
+            .statusCode(200);
+    }
+
+//c3025ece9f7715439cf26cc7cadb94244ca633b999ebb4fdae247c067ad64771
+
+@Test
+public void testListUsers() {
+    given()
+        .header("Accept", "application/json")
+        .header("Content-Type", "application/json")
+        .header("Authorization", "Bearer " + ACCESS_TOKEN)
+    .when()
+        .get(BASE_URL + "/users")
+    .then()
+        .statusCode(200);
+}
+
+}
