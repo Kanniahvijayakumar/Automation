7b9a40fa4ba266bf3dee906ff1eee1c15e954c3f
vijayx21188955 <x21188955@student.ncirl.ie>
2024-06-07 23:21:06 +0100

    Adding New Files

--- src/test/java/excelRead/ReadTestCaseName.java
+++ src/test/java/excelRead/ReadTestCaseName.java
@@ -2,163 +2,106 @@ package excelRead;
 
 import org.apache.poi.ss.usermodel.Sheet;
 
+//import excelreaders.ReadExcel;
+//import excelreaders.ReadTestCaseName;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 
 public class ReadTestCaseName {
 	
-//	public ArrayList<String> getTestCaseDetails(String testCaseName) throws IOException {
+    private static final Logger logger = LogManager.getLogger(ReadTestCaseName.class);
 
-//		New code 
-		
-		public Map<String, Map<String, String>> getTestCaseDetails(String testCaseName) throws IOException {
-		
-//		New code
-	
-	ReadExcel file = new ReadExcel();
-	
-	Sheet guru99Sheet = file.readExcel("C:\\Users\\kanni\\OneDrive\\Desktop\\Interview\\FrameWork\\src\\main\\resources\\ExcelData"+"\\","TestCase.xlsx" , "KeywordFramework");
-	
-	ArrayList<String> cars = new ArrayList<String>();
-	
-	/* New code */
-	
-	Map<String, Map<String, String>> testCaseMap = new LinkedHashMap<>();
-	
+    /**
+     * Reads test case details from the specified Excel sheet.
+     *
+     * @param testCaseName the name of the test case to read details for
+     * @return a map containing the test case details
+     * @throws IOException if there is an issue reading the Excel file
+     */
+    public Map<String, Map<String, String>> getTestCaseDetails(String testCaseName, String sheetname) throws IOException {
 
-	
-	/* New code */
-	
-	cars.clear();
-	
-	//Find number of rows in excel file
-    int rowCount = guru99Sheet.getLastRowNum()-guru99Sheet.getFirstRowNum();
+        ReadExcel file = new ReadExcel();
+        
+        String projectDir = System.getProperty("user.dir");
+        // Read the specified Excel sheet
+        Sheet guru99Sheet = file.readExcel(projectDir + "\\src\\main\\resources\\ExcelData\\", "TestCase.xlsx", "KeywordFramework");
+
+        Map<String, Map<String, String>> testCaseMap = new LinkedHashMap<>();
+
+        // Find the number of rows in the Excel file
+        int rowCount = guru99Sheet.getLastRowNum() - guru99Sheet.getFirstRowNum();
+
+        String testcasename = "";
+        String actualTestcasename = "";
+
+        // Create a loop over all the rows of the Excel file to read it
+        for (int i = 1; i <= rowCount; i++) {
+            Row row = guru99Sheet.getRow(i);
+            Map<String, String> detailsMap = new LinkedHashMap<>();
+
+            // Check if the first cell contains a value, indicating a new test case name
+            if (row.getCell(0).toString().length() != 0) {
+                testcasename = row.getCell(0).toString();
+                logger.info("New Testcase -> {} Started", testcasename);
+            } else {
+                // If the current row belongs to the specified test case
+                if (testcasename.equals(testCaseName)) {
+                    if (actualTestcasename.isEmpty()) {
+                        actualTestcasename = testcasename;
+                    }
+
+                    if (row.getCell(0).toString().length() == 0 && testcasename.equalsIgnoreCase(testCaseName)) {
+                        // Log the test case details
+                        logger.debug("Step details: {} ---- {} ---- {} ---- {}", row.getCell(1), row.getCell(2), row.getCell(3), row.getCell(4));
+
+                        // Populate the details map with test case step details
+                        detailsMap.put("Keyword", row.getCell(1).toString());
+                        detailsMap.put("Object", row.getCell(2).toString());
+                        detailsMap.put("ObjectType", row.getCell(3).toString());
+                        detailsMap.put("value", row.getCell(4).toString());
+
+                        String stepKey = "Step" + i;
+                        testCaseMap.put(stepKey, detailsMap);
+                    }
+                }
+            }
+        }
+
+        logger.info("Test case details extraction complete");
+        return testCaseMap;
+    }
     
-    boolean flag = false;
-    
-    
-    
-    String testcasename ="";
-    
-    String actualTestcasename ="";
-    
-	  //Create a loop over all the rows of excel file to read it
-  for (int i = 1; i < rowCount+1; i++) {
-	  
-		Map<String, String> detailsMap = new LinkedHashMap<>();
-      //Loop over all the rows
-      Row row = guru99Sheet.getRow(i);
-      //Check if the first cell contain a value, if yes, That means it is the new testcase name
-      
-   
-      
-      if(row.getCell(0).toString().length()!=0){
-      	
-      	System.out.println("New Testcase->"+row.getCell(0).toString() +" Started");
-      	
-      	testcasename = row.getCell(0).toString(); 
-      	
-      	System.out.print(testcasename);
-          	
+    public String getJiraId(String testCaseName, String sheetName) throws IOException {
+        ReadExcel file = new ReadExcel();
+        String projectDir = System.getProperty("user.dir");
+        Sheet sheet = file.readExcel(projectDir + "\\src\\main\\resources\\ExcelData\\", "TestCase.xlsx", sheetName);
 
-       
-      	
-      }
+        int rowCount = sheet.getLastRowNum() - sheet.getFirstRowNum();
+        String testCaseId = "";
 
-      else{
-          //Print the new testcase name when it started
-      	if(testcasename.equals(testCaseName)){
-      		
-      		
-      		
-      		System.out.print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-      		
-      				if(actualTestcasename.isEmpty()) {
-      					
-      					actualTestcasename = testcasename;
-      					
-      					System.out.print("DDDDDDDDDDDDDDDDDDDDDDDD");
-      					
-      					cars.add(testcasename);
-      					
-//      					New code 
-      					
-//      					Map<String, String> detailsMap = new HashMap<>();
-//                        detailsMap.put("Test Case Name", testcasename);
-//      					
-//      					testCaseMap.put(testcasename, detailsMap);
-      					
-//      					New Code
-      					
-      				}
-              	
-	    	        if((row.getCell(0).toString().length()==0) && (testcasename.equalsIgnoreCase(testCaseName))){
-	    		        //Print testcase detail on console
-	    		            System.out.println(row.getCell(1).toString()+"----"+ row.getCell(2).toString()+"----"+
-	    		            row.getCell(3).toString()+"----"+ row.getCell(4).toString());
-	    		            
-	    		            System.out.print("Hi");
-	    		            
-//	    		            New code
-	    		            
-//	    		            Map<String, String> detailsMap = testCaseMap.get(testcasename);
-	    		            
-	                        detailsMap.put("Keyword", row.getCell(1).toString());
-	                        detailsMap.put("Object", row.getCell(2).toString());
-	                        detailsMap.put("ObjectType", row.getCell(3).toString());
-	                        detailsMap.put("value", row.getCell(4).toString());
-	                        
-	                        String q = "Steps" + i;
-	                        
-	                        testCaseMap.put(q, detailsMap);
-	                        
-	                        System.out.println("QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ");
-	                        
-	                        System.out.println(testCaseMap);
-	                        
-//	    		            New Code
-	    		            
-	    		            cars.add(row.getCell(1).toString());
-	    		            cars.add(row.getCell(2).toString());
-	    		            cars.add(row.getCell(3).toString());
-	    		            cars.add(row.getCell(4).toString());
-	    		            
-	    		      
-	    		            
-	    		        }
-              	
-              	
+        for (int i = 1; i <= rowCount; i++) {
+            Row row = sheet.getRow(i);
 
-              
+            // Check if the row belongs to the specified test case
+            if (row.getCell(0) != null && row.getCell(0).toString().equals(testCaseName)) {
+                // Check if the row contains a Jira ID
+                if (row.getCell(5) != null && !row.getCell(5).toString().isEmpty()) {
+                    testCaseId = row.getCell(5).toString();
+                    break;
+                }
+            }
+        }
 
-  }
-  }
-    
-    
-    
-    
-    
-
-
-}
-//return cars;
-
-//New Code
-  
- System.out.print("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
- 
- System.out.print("testCaseMap");
-
-return testCaseMap;
-
-//New Code
-
-}
+        return testCaseId;
+    }
 	
 }
